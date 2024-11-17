b911da7edeef74294b16a14fbcc445d456ddcd19
Phawarit231149 <phavarisyatniyom@gmail.com>
2024-11-17 14:44:54 +0700

    Update 494.py

--- 494.py
+++ 494.py
@@ -3,6 +3,6 @@ for i in range(1,n+1,1):
     for j in range (1,n+1,1):
         if i+j >= n+1:
             print ("*",end="")
-        else:
+        elif i+j < n+1:
             print("-",end="")
     print()
